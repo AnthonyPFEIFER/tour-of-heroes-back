5dcfd00d9d4bf9fc45766ac057c308a783564e53
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2022-09-29 15:48:45 +0200

    suppression data sql

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into hero(id, name) VALUES (1, 'Canada');
-insert into hero(id, name) values(2, 'Yes Man');
-insert into hero(id, name) values(3, 'Bombastico');
-insert into hero(id, name) values(4, 'Fennel');
-insert into hero(id, name) values(5, 'Magnostic');
-insert into hero(id, name) values(6, 'Duck Man');
-insert into hero(id, name) values(7, 'Silly Putty Man');
-insert into hero(id, name) values(8, 'Company Man');
